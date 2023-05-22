ef3e79fd3326f75fa19b80e904a4535eafe1fb4c
Obijuan <obijuan.cube@gmail.com>
2023-05-22 11:44:04 +0200

    Update 01-leds-on.s: Kianv Bug removed (Thanks to @splinedrive)

--- Firmware/01-rars-asm/01-leds-on.s
+++ Firmware/01-rars-asm/01-leds-on.s
@@ -1,27 +1,21 @@
-#-- Programa para enviar un valor a los LEDs
+#-- Write a value to the LEDs
 
+    #-- Addres were LEDs are mapped
 	.eqv LEDS 0x12000000
+
+	#-- Value to write to the LEDs
 	.eqv VALUE 0x03
 	
 	.text
 	
-	#-- Las primeras 9 instrucciones (antes de la direccion 0x24) 
-	#-- no se ejecutan por un BUG en KianV...
-	nop   #-- 0
-	nop   #-- 4
-	nop   #-- 8
-	nop   #-- C
-	nop   #-- 10
-	nop   #-- 14
-	nop   #-- 18
-	nop   #-- 1c
-	nop   #-- 20
-	
-	#-- Primer instruccion que se ejecuta
-	#-- Puntero a la uart
+	#-- Pointer for accessing leds
 	li t0, LEDS
-	li t1, 0x03
+
+	#-- Send VALUE to LEDs
+	li t1, VALUE
 	sw t1, 0(t0)
+
+	#-- Stop!
 inf:	b inf
 	
 	
